40f819fc5b32ea9e088a4af9953111bd032a8ed2
jinho6225 <jinho6225@hotmail.com>
2020-12-25 16:10:53 -0800

    added profile pic, applied tailwind

---
+++ components/layout.js
@@ -0,0 +1,45 @@
+import Head from 'next/head'
+import Link from 'next/link'
+
+const name = 'Jinho6225'
+export const siteTitle = 'Devlog'
+
+export default function Layout({ children, home }) {
+    return (
+    <div className="max-w-xl py-0 px-4 mx-auto mt-12 mb-24">
+        <Head>
+            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            <meta
+            name="description"
+            content="Developer Blog using Next.js"
+            />
+            <meta name="og:title" content={siteTitle} />
+        </Head>
+        <header className="flex flex-col items-center justify-center">
+            {home ? (
+            <>
+                <img
+                src="/images/profile.jpg"
+                className="rounded-full w-32"
+                alt={name}
+                />
+                <h1 className="text-4xl font-bold mt-4">{name}</h1>
+            </>
+            ) : (
+            <>
+                <Link href="/">
+                <a>
+                    <img
+                    src="/images/profile.jpg"
+                    className="rounded-full w-32"
+                    alt={name}
+                    />
+                </a>
+                </Link>
+            </>
+            )}
+        </header>
+        <main>{children}</main>
+    </div>
+    )
+}
